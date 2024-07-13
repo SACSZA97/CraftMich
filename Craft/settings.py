db233cf964f2e08887e91f452a6827748ab833bd
SACSZA97 <134246985+SACSZA97@users.noreply.github.com>
2024-07-13 14:28:23 -0600

    Mary
    Se agregaron los estilos al administrador  de manera que concordaran con el proyecto en respecto a la tarea del día lunes

--- Craft/settings.py
+++ Craft/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'admin_interface',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,8 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'michoacan',
-    'base.apps.BaseConfig'
+    'base.apps.BaseConfig',
+    'crispy_forms',
+    'ckeditor',
+    'colorfield',
 ]
+X_FRAME_OPTIONS='SAMEORIGIN'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -56,7 +61,7 @@ ROOT_URLCONF = 'Craft.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR,'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -105,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-mx'
 
 TIME_ZONE = 'UTC'
 
@@ -126,3 +131,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 import os
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+LOGIN_URL = '/login/'
+LOGIN_REDIRECT_URL = '/'
--- Craft/urls.py
+++ Craft/urls.py
@@ -15,15 +15,34 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from michoacan import views
 from django.conf import settings
 from base import views as views_base
+from django.conf.urls.static import static
+from michoacan import views as views_mich
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('ca/', views.ca, name="ca"),
-    path('', views_base.Carrusel, name="Carrusel")
+    path('producto/',views_base.producto,name="Producto"),
+    path('eliminarCarrusel/<int:id>/',views_base.eliminarCarrusel, name='Eliminarcarrusel'),
+    path('editarCarrusel/<int:id>/',views_base.editarCarrusel, name='Editarcarrusel'),
+    path('mascarrusel/',views_base.mascarrusel,name="Mascarrusel"),
+    
+    path('bienvenida/', views.bienvenida, name="bienvenida"),
+    path('Quienessomos/', views.Quienessomos, name="Quienessomos"),
+    path('registro/', views_base.registro, name="Registro"),
+    path('login/', views_base.login, name="Login"),
+    path('', views_base.Carrusel, name="Carrusel"),
+    path('accounts', include('django.contrib.auth.urls')),
+    path('CrearEvento/', views_mich.CrearEventos, name="CrearEvento"),
+    path('ConsultarEvento/', views_mich.ConsultarEvento, name="ConsultarEvento"),
+    path('eliminarEvento/<int:id>/',views_mich.eliminarEvento,name='Eliminar'),
+    path('formEditarEvento/<int:id>/',views_mich.ConsultarEventoIndividual, name='ConsultaIndividual'),
+    path('editarEvento/<int:id>/',views_mich.editarEvento,name='Editar'),
+    path('Contactos/',views_base.Contactos,name="Contactos"),
+     path('eventos', views_mich.eventos, name="eventos"),
 ]
 
 if settings.DEBUG:
